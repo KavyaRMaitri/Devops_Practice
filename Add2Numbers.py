c030b114f9b4f0fde80891334cad852a09c29fd6
KavyaRMaitri <96657421+KavyaRMaitri@users.noreply.github.com>
2024-04-27 15:44:15 +0530

    Update Add2Numbers.py

--- Add2Numbers.py
+++ Add2Numbers.py
@@ -1,7 +1,2 @@
-def add_numbers(num1, num2):
-    return(num1 + num2)
-
-    result = add_numbers(num1, num2)
-
-    print("The sum of 2 numbers is:",result)
-
+add = num1+num2
+print("The sum of 2 numbers is: ",add)
