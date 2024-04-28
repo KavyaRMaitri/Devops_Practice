9eaa6f321dcd951b5a7edca20d500ed5c6fd1142
KavyaRMaitri <96657421+KavyaRMaitri@users.noreply.github.com>
2024-04-28 20:06:59 +0530

    Delete S1.py

--- S1.py
+++
@@ -1,9 +0,0 @@
-def add_numbers(num1, num2):
-    return num1 + num2
-
-if __name__ == "__main__":
-    import sys
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[2])
-    result = add_numbers(num1, num2)
-    print("The sum of", num1, "and", num2, "is:", result)
