9b1d34914e424bfd6af716064e240cc076b515ce
KavyaRMaitri <96657421+KavyaRMaitri@users.noreply.github.com>
2024-04-28 16:20:11 +0530

    Update S1.py

--- S1.py
+++ S1.py
@@ -5,8 +5,8 @@ def add_numbers(num1, num2):
 
 if __name__ == "__main__":
     # Fetching input from Jenkins secrets
-    num1 = int(os.environ['secret1'])
-    num2 = int(os.environ['secret2'])
+    num1 = int(os.environ.get('secret1'))
+    num2 = int(os.environ.get('secret2'))
     
     # Adding the numbers
     result = add_numbers(num1, num2)
