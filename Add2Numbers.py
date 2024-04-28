d7bac2ee74ff6f070e61eab71fb80037c0a1efee
KavyaRMaitri <96657421+KavyaRMaitri@users.noreply.github.com>
2024-04-28 13:41:58 +0530

    Update Add2Numbers.py

--- Add2Numbers.py
+++ Add2Numbers.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
    num1 = float(sys.argv[1])
    num2 = float(sys.argv[2])
    
-  result = add_numbers(num1, num2)
-  print ("The sum of", num1, "and", num2, "is:", result) 
+   result = add_numbers(num1, num2)
+   print ("The sum of", num1, "and", num2, "is:", result) 
