8347981151939983b9cf86324e78a00f332f2c49
KavyaRMaitri <96657421+KavyaRMaitri@users.noreply.github.com>
2024-04-28 14:37:38 +0530

    Update Secrete.py

--- Secrete.py
+++ Secrete.py
@@ -5,8 +5,8 @@ def add_numbers(num1, num2):
 
 if __name__ == "__main__":
     
-    num1 = int(os.environ.get('NUM1'))
-    num2 = int(os.environ.get('NUM2'))
+    num1 = int(os.environ.get('num1'))
+    num2 = int(os.environ.get('num2'))
     
     result = add_numbers(num1, num2)
     print("The sum of", num1, "and", num2, "is:", result)
