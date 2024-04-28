9129fcfe972e6cb36625717f5ce0f42f1634def7
KavyaRMaitri <96657421+KavyaRMaitri@users.noreply.github.com>
2024-04-28 13:14:46 +0530

    Update Add2Numbers.py

--- Add2Numbers.py
+++ Add2Numbers.py
@@ -2,7 +2,7 @@ def add_numbers(num1, num2):
   return num1 + num2
 
 if __name__ == "__main__":
-  num1 = float(input("Enter the first number:"))
-  num2 = float(input("Enter the second number:"))
+  num1 = float(input("Enter the first number: $num1"))
+  num2 = float(input("Enter the second number: $num2"))
   result = add_numbers(num1, num2)
   print ("The sum of", num1, "and", num2, "is:", result) 
